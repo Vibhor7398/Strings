0da87cc88c86074c2930790c733f62f353908539
Vibhor Gulati <vibhorgulati7@gmail.com>
2018-06-15 20:16:43 +0530

    Second Commit

--- removevowels.java
+++ removevowels.java
@@ -8,7 +8,8 @@ public class removevowels {
 		String result = "", temp = "";
 		for (int i = 0; i < str.length(); i++) {
 			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
-					|| str.charAt(i) == 'u') {
+					|| str.charAt(i) == 'u'||str.charAt(i) == 'A' || str.charAt(i) == 'E' || str.charAt(i) == 'I' || str.charAt(i) == 'O'
+					|| str.charAt(i) == 'U') {
 
 			} else {
 				temp += str.charAt(i);
